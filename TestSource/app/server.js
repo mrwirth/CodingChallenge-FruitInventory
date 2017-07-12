9351fd70cf56a857ead308774c7d92acb4b7ad68
MR Wirth <wirth.mr+github@gmail.com>
2017-07-12 08:36:30 -0400

    Add first version of challenge.
    Contains simple static text source as well as dynamic text source for
    the challenge.

--- TestSource/app/server.js
+++ TestSource/app/server.js
@@ -4,6 +4,26 @@
 // init project
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+if (!String.prototype.padEnd) {
+    String.prototype.padEnd = function padEnd(targetLength,padString) {
+        targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+        padString = String(padString || ' ');
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return String(this) + padString.slice(0,targetLength);
+        }
+    };
+}
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -16,22 +36,77 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/sample-data.txt", function (request, response) {
-  response.sendFile(__dirname + '/Sample-Data.txt');
+app.get("/dynamic-data.txt", function (request, response) {
+  response.set({"Content-Type":"text/plain; charset=UTF-8"});
+  response.send(dynamicData());
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
+var dynamicData = function() {
+  var fruits = fs.readFileSync('fruits.txt', 'utf8');
+  fruits = fruits.split('\n');
+  fruits = shuffle(fruits);
+  var n = Math.floor(Math.random() * (25)) + 8; // arbitrary sizes, nothing special.
+  fruits = fruits.slice(0, n);
+  fruits = fruits.concat(fruits.slice(0, 3)); //take 3 duplicates.  Yes, another arbitrary number.
+  fruits = shuffle(fruits);
+  //build inventory rows
+  var minNameLength = fruits.reduce(function(maxLength, fruit) {
+    return fruit.length > maxLength ? fruit.length : maxLength;
+  }, 0) + 2;
+  var columns = createColumns(minNameLength);
+  var inventory = fruits.map(createRow(columns));
+  return columns.header + inventory.join('\n');
+}
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+// From https://bost.ocks.org/mike/shuffle/
+var shuffle = function(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+var createColumns = function (minNameLength) {
+  // each length has two added onto the end to ensure each column is
+  // separated by at least a double column of spaces.
+  var nameLength = minNameLength + Math.floor(Math.random() * 11) + 2;
+  var amountLength = 6 + 1 + 2 + Math.floor(Math.random() * 11) + 2; // ###### + " " + "kg"
+  var priceLength = 1 + 6 + Math.floor(Math.random() * 11) + 2; // "$" + ######
+  var header = "Product name".padEnd(nameLength) + "Amount".padEnd(amountLength) + "Price".padEnd(priceLength);
+  header = header + "\n" + "=".repeat(nameLength+amountLength+priceLength) + "\n";
+
+  return {
+    header: header,
+    nameLength: nameLength,
+    amountLength: amountLength,
+    priceLength: priceLength
+  };
+}
+
+var createRow = function (columns) {
+  var nameLength = columns.nameLength;
+  var amountLength = columns.amountLength;
+  var priceLength = columns.priceLength;
+  return function(fruit) {
+    var amount = Math.floor(Math.random() * 100000) / 100;
+    var amountUnit = Math.floor(Math.random() * 2) >= 1 ? "kg" : "g";
+    var amountUnit = (Math.floor(Math.random() * 2) >= 1 ? " " : "") + amountUnit;
+    var price = Math.floor(Math.random() * 100000) / 100;
+
+    return fruit.padEnd(nameLength) + (amount + amountUnit).padEnd(amountLength) + ("$" + price).padEnd(priceLength);
+  }
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
