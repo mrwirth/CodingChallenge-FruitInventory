0b893849457a5e3aca6995052339a121ebeb39f5
MR Wirth <wirth.mr+github@gmail.com>
2017-07-12 02:04:21 -0400

    Complete console frontend for `CSharpFruits`.

--- CSharpFruits/CSharpFruits/Program.cs
+++ CSharpFruits/CSharpFruits/Program.cs
@@ -16,7 +16,15 @@ namespace CSharpFruits
         static void Main(string[] args)
         {
             var dataUrl = ConfigurationManager.AppSettings["dataUrl"];
-            var fruits = Fruits.GetFruits(dataUrl);
+            var fruits = from fruit in Fruits.GetFruits(dataUrl).DeduplicateBy(x => x.Name)
+                         where fruit.AmountInGrams > 750
+                         orderby fruit.Price descending
+                         select fruit;
+            foreach (var fruit in fruits)
+            {
+                Console.WriteLine($"{fruit.Name}\t${fruit.Price:#,0.00#}\t{fruit.Amount:#,0.00#} {fruit.AmountUnit}");
+            }
+            Console.ReadLine();
         }
     }
 }
