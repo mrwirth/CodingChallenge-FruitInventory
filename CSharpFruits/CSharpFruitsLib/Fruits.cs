d32f3910a336e666bd9319a6ec370ac9b684d131
MR Wirth <wirth.mr+github@gmail.com>
2017-07-12 02:03:18 -0400

    Complete first version of `CSharpFruitsLib`.

--- CSharpFruits/CSharpFruitsLib/Fruits.cs
+++ CSharpFruits/CSharpFruitsLib/Fruits.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CSharpFruitsLib
@@ -10,17 +11,17 @@ namespace CSharpFruitsLib
     {
         public string Name { get; }
         public decimal Price { get; }
-        decimal Amount { get; }
-        string AmountUnit { get; }
-        decimal NormalizedAmount { get; }
+        public decimal Amount { get; }
+        public string AmountUnit { get; }
+        public decimal AmountInGrams { get; }
 
-        public Fruit(string name, decimal price, string priceUnit, decimal amount, string amountUnit)
+        public Fruit(string name, decimal price, decimal amount, string amountUnit)
         {
             Name = name;
             Price = price;
             Amount = amount;
             AmountUnit = amountUnit;
-            NormalizedAmount = NormalizeWeight(amount, amountUnit);
+            AmountInGrams = NormalizeWeight(amount, amountUnit);
         }
 
         private static decimal NormalizeWeight(decimal amount, string amountUnit)
@@ -29,13 +30,83 @@ namespace CSharpFruitsLib
             if (amountUnit == "kg") { return amount * 1000; };
             throw new ArgumentOutOfRangeException("amountUnit", amountUnit, "Only g and kg are currently supported.");
         }
+
+        public override string ToString()
+        {
+            return Name + "\t$" + Price + "\t" + Amount + " " + AmountUnit;
+        }
     }
 
     public static class Fruits
     {
         public static IEnumerable<Fruit> GetFruits(string url)
         {
-            throw new NotImplementedException();
+            return FetchData(url).SplitData().ParseData();
+        }
+
+        private static string FetchData(string url)
+        {
+            using (var client = new System.Net.WebClient())
+            {
+                return client.DownloadString(url);
+            }
+        }
+
+        private static (string, IEnumerable<string>) SplitData(this string data)
+        {
+            var rows = data.Split('\n');
+            var headerRow = rows[0];
+            var dataRows = rows.Skip(2);
+            return (headerRow, dataRows);
+        }
+
+        private static IEnumerable<Fruit> ParseData(this (string header, IEnumerable<string> rows) data)
+        {
+            var names = FindIndexes("Product name", data.header);
+            var prices = FindIndexes("Unit price", data.header);
+            var amounts = FindIndexes("Amount", data.header);
+
+            var numberPattern = new Regex(@"[0-9.]+");
+            var unitPattern = new Regex(@"[A-Za-z]+");
+            foreach (var row in data.rows)
+            {
+                var name = row.Substring(names.start, names.length).Trim();
+                var amountText = row.Substring(amounts.start, amounts.length).Trim();
+                var amount = decimal.Parse(numberPattern.Match(amountText).Value);
+                var amountUnit = unitPattern.Match(amountText).Value.Trim();
+                var priceText = row.Substring(prices.start, prices.length).Trim();
+                var price = decimal.Parse(numberPattern.Match(priceText).Value);
+                yield return new Fruit(name, price, amount, amountUnit);
+            }
+            yield break;
+        }
+
+        private static (int start, int length) FindIndexes(string columnName, string header)
+        {
+            var pattern = new Regex(columnName + @"\s+");
+            var match = pattern.Match(header);
+            var start = match.Index;
+            var length = match.Length;
+            return (start, length);
+        }
+
+        public static IEnumerable<Fruit> DeduplicateBy<T>(this IEnumerable<Fruit> fruits, Func<Fruit, T> key)
+        {
+            var seen = new HashSet<T>();
+            foreach (var fruit in fruits)
+            {
+                if (!seen.Contains(key(fruit)))
+                {
+                    seen.Add(key(fruit));
+                    yield return fruit;
+                }
+            }
+            yield break;
+        }
+
+        public static IEnumerable<Fruit> Deduplicate(this IEnumerable<Fruit> fruits)
+        {
+            return fruits.DeduplicateBy(x => x);
         }
     }
 }
