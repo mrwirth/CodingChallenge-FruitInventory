e9320fdde02181ca78e22241682fe68ad676ee06
MR Wirth <wirth.mr+github@gmail.com>
2017-07-11 22:47:54 -0400

    Add `Fruit` class and static `Fruits` utility class.

--- CSharpFruits/CSharpFruitsLib/Fruits.cs
+++ CSharpFruits/CSharpFruitsLib/Fruits.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace CSharpFruitsLib
 {
-    public class Class1
+    public class Fruit
     {
+        public string Name { get; }
+        public decimal Price { get; }
+        decimal Amount { get; }
+        string AmountUnit { get; }
+        decimal NormalizedAmount { get; }
+
+        public Fruit(string name, decimal price, string priceUnit, decimal amount, string amountUnit)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+            AmountUnit = amountUnit;
+            NormalizedAmount = NormalizeWeight(amount, amountUnit);
+        }
+
+        private static decimal NormalizeWeight(decimal amount, string amountUnit)
+        {
+            if (amountUnit == "g") { return amount; };
+            if (amountUnit == "kg") { return amount * 1000; };
+            throw new ArgumentOutOfRangeException("amountUnit", amountUnit, "Only g and kg are currently supported.");
+        }
+    }
+
+    public static class Fruits
+    {
+        public static IEnumerable<Fruit> GetFruits(string url)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
