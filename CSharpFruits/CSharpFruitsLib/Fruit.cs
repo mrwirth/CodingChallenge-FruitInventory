69589622a1de40fb160183131e2d844bfece1adc
MR Wirth <wirth.mr+github@gmail.com>
2017-07-23 11:29:26 -0400

    Split `Fruit` class out of `Fruits.cs`.

---
+++ CSharpFruits/CSharpFruitsLib/Fruit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharpFruitsLib
+{
+    public class Fruit
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+        public decimal Amount { get; }
+        public string AmountUnit { get; }
+        public decimal AmountInGrams { get; }
+
+        public Fruit(string name, decimal price, decimal amount, string amountUnit)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+            AmountUnit = amountUnit;
+            AmountInGrams = NormalizeWeight(amount, amountUnit);
+        }
+
+        private static decimal NormalizeWeight(decimal amount, string amountUnit)
+        {
+            if (amountUnit == "g") { return amount; };
+            if (amountUnit == "kg") { return amount * 1000; };
+            throw new ArgumentOutOfRangeException("amountUnit", amountUnit, "Only g and kg are currently supported.");
+        }
+
+        public override string ToString()
+        {
+            return Name + "\t$" + Price + "\t" + Amount + " " + AmountUnit;
+        }
+    }
+}
--- CSharpFruits/CSharpFruitsLib/Fruits.cs
+++ CSharpFruits/CSharpFruitsLib/Fruits.cs
@@ -7,36 +7,6 @@ using System.Threading.Tasks;
 
 namespace CSharpFruitsLib
 {
-    public class Fruit
-    {
-        public string Name { get; }
-        public decimal Price { get; }
-        public decimal Amount { get; }
-        public string AmountUnit { get; }
-        public decimal AmountInGrams { get; }
-
-        public Fruit(string name, decimal price, decimal amount, string amountUnit)
-        {
-            Name = name;
-            Price = price;
-            Amount = amount;
-            AmountUnit = amountUnit;
-            AmountInGrams = NormalizeWeight(amount, amountUnit);
-        }
-
-        private static decimal NormalizeWeight(decimal amount, string amountUnit)
-        {
-            if (amountUnit == "g") { return amount; };
-            if (amountUnit == "kg") { return amount * 1000; };
-            throw new ArgumentOutOfRangeException("amountUnit", amountUnit, "Only g and kg are currently supported.");
-        }
-
-        public override string ToString()
-        {
-            return Name + "\t$" + Price + "\t" + Amount + " " + AmountUnit;
-        }
-    }
-
     public static class Fruits
     {
         public static IEnumerable<Fruit> GetFruits(string url)
