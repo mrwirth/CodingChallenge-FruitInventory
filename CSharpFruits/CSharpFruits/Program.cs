fb8924cae83bbbb395a0211993ed857a214ecc58
MR Wirth <wirth.mr@gmail.com>
2019-04-04 22:42:52 -0400

    Calculate max fruit name length instead of assuming it.

--- CSharpFruits/CSharpFruits/Program.cs
+++ CSharpFruits/CSharpFruits/Program.cs
@@ -19,11 +19,14 @@ namespace CSharpFruits
                 .DistinctBy(x => x.Name)
                 .Where(x => x.Price >= 30.00m)
                 .OrderByDescending(x => x.AmountInGrams);
+            var maxNameLength =
+                fruits
+                .Select(x => x.Name.Length)
+                .Max();
             foreach (var fruit in fruits)
             {
-                // Slight cheating here: I know the length of the longest fruit name,
-                // so I'm padding by enough to make the console output easy to read.
-                Console.WriteLine($"{fruit.Name,20}\t${fruit.Price:#,0.00#}\t{fruit.Amount:#,0.00#} {fruit.AmountUnit}");
+                var printName = fruit.Name.PadLeft(maxNameLength);
+                Console.WriteLine($"{printName}\t${fruit.Price:#,0.00#}\t{fruit.Amount:#,0.00#} {fruit.AmountUnit}");
             }
             Console.ReadLine();
         }
